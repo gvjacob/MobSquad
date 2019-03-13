3e376fa74a83153a51f1994fc1bb8f0f163eec6d
gvjacob <gvjacob@outlook.com>
2019-03-13 17:14:32 -0400

    Initial Commit

---
+++ MobSquad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MobSquad
+//
+//  Created by Gino V. Jacob on 3/13/19.
+//  Copyright © 2019 Gino V. Jacob. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
